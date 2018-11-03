e20f196de38eb8a9f93c25df88e6d6e09749349f
MasterMindB83 <44545411+MasterMindB83@users.noreply.github.com>
2018-11-03 11:16:05 +0100

    working

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  url$: string;
+  constructor(private http: HttpClient) {  }
+  getWeather(city) {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' +
+    city + '&appid=cdc1b906ec2ba962977a603b61686fc4');
+  }
 }
--- src/app/weathar/weathar.component.ts
+++ src/app/weathar/weathar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {DataService} from '../data.service';
+import {Observable} from 'rxjs';
+import {IWeather} from '../interfaces';
 
 @Component({
   selector: 'app-weathar',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weathar.component.css']
 })
 export class WeatharComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  currentWeather$: IWeather;
+  date: number;
+  temp: number;
+  city: string;
+  sunrise: number;
+  sunset: number;
+    constructor(private data: DataService) {
+      // this.city = 'Nis';
+      this.currentWeather$ = {coord: {lon: 0, lat: 0},
+      weather: {id: 0, main: '', description: '', icon: 'o1n'},
+      base: '',
+      main: {temp: 0, pressure: 0, humidity: 0, temp_min: 0, temp_max: 0},
+      visibility: 0,
+      wind: {speed: 0},
+      clouds: {all: 0},
+      dt: 0,
+      sys: {type: 0, id: 0, message: 0, country: '', sunrise: 0, sunset: 0},
+      id: 0,
+      name: '',
+      cod: 0
+    };
   }
-
+  ngOnInit() {
+    /*this.city = 'Nis';
+     this.getWeather();*/
+  }
+  getWeather() {
+    /*this.data.getWeather(this.city).subscribe((data: IWeather) => this.currentWeather$ = data);
+    if (this.currentWeather$.dt > 0) {
+      this.formatData();
+    }*/
+  }
+  kalvinToCelzious(temp: number) {
+  }
+  formatData() {
+    this.date = this.currentWeather$.dt * 1000;
+    this.temp = this.currentWeather$.main.temp - 275.15;
+    this.sunrise = this.currentWeather$.sys.sunrise * 1000;
+    this.sunset = this.currentWeather$.sys.sunset * 1000;
+  }
 }
